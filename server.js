d345205176aed07ea6c2795433f116fcf67a6616
HVKG2727 <vinodkanna27221213@gmail.com>
2022-09-09 11:38:04 +0530

    change3 in server.js

--- server.js
+++ server.js
@@ -24,10 +24,8 @@ app.use(cookieParser(process.env.REF_TOKEN_SECRET));
 app.use(helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"],
-       // scriptSrcElem: ["'self'", "'unsafe-inline'"],
-        //styleSrc: ["'slef'", "'unsafe-inline'"],
-        connectSrc: ['www.googleapis.com'],
-        imgSrc: ["'self'", '*.unsplash.com', '*.google.com']
+        styleSrc: ["'self'"],
+        imgSrc: ["'self"]
     }
 }));
 app.use(fileUpload({
